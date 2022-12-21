264338b4549c94dcf0f267ff9740132887fe74a2
kennedybailey <121199666+kennedybailey@users.noreply.github.com>
2022-12-21 16:47:14 -0500

    Uploaded Weather Project into Git

---
+++ src/app.js
@@ -0,0 +1,43 @@
+let apiKey = "2980ff43226d67e53abfcdb6d457dcc8";
+
+let dateElement = document.querySelector("#date");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thurday",
+  "Friday",
+  "Saturday",
+];
+
+let now = new Date();
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+dateElement.innerHTML = `${day} ${hours}:${minutes}`;
+
+function searchCity(event) {
+  event.preventDefault();
+  let input = document.querySelector("#search-bar");
+  let h1 = document.querySelector("h1");
+  let capitalizeCity =
+    input.value.charAt(0).toUpperCase() + input.value.slice(1);
+  h1.innerHTML = `${capitalizeCity}`;
+
+  function changeInfo(response) {
+    let currentTemp = Math.round(response.data.main.temp);
+    let feelsLike = Math.round(response.data.main.feels_like);
+    let changeCurrentTemp = document.querySelector("#current-temp");
+    let changeFeelsLike = document.querySelector("#feels-like-temp");
+    changeCurrentTemp.innerHTML = `${currentTemp}°C`;
+    changeFeelsLike.innerHTML = `${feelsLike}°C`;
+  }
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capitalizeCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(changeInfo);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", searchCity);
