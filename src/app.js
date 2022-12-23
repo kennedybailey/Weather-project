753490f984d5357bef8abd70906e0b0d9f5e0111
kennedybailey <121199666+kennedybailey@users.noreply.github.com>
2022-12-22 22:05:36 -0500

    JS for weather unit conversions and some CSS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 let apiKey = "2980ff43226d67e53abfcdb6d457dcc8";
 
+//date
 let dateElement = document.querySelector("#date");
 
 let days = [
@@ -19,6 +20,7 @@ let minutes = now.getMinutes();
 
 dateElement.innerHTML = `${day} ${hours}:${minutes}`;
 
+//search city
 function searchCity(event) {
   event.preventDefault();
   let input = document.querySelector("#search-bar");
@@ -38,14 +40,76 @@ function searchCity(event) {
     let changeCurrentDescription = document.querySelector(
       "#weather-description"
     );
-    changeCurrentTemp.innerHTML = `${currentTemp}°C`;
-    changeFeelsLike.innerHTML = `${feelsLike}°C`;
-    changeCurrentWind.innerHTML = `Wind Speed: ${currentWind} km/h`;
+    if (apiUnit === "imperial") {
+      changeCurrentTemp.innerHTML = `${currentTemp}°F`;
+      changeFeelsLike.innerHTML = `${feelsLike}°F`;
+      changeCurrentWind.innerHTML = `${currentWind} mph`;
+    } else {
+      changeCurrentTemp.innerHTML = `${currentTemp}°C`;
+      changeFeelsLike.innerHTML = `${feelsLike}°C`;
+      changeCurrentWind.innerHTML = `${currentWind} km/h`;
+    }
+
     changeCurrentDescription.innerHTML = `${currentDescription}`;
   }
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capitalizeCity}&appid=${apiKey}&units=metric`;
+  let changeToUnit = document.querySelector("#temp-change").innerHTML;
+  let apiUnit = "metric";
+  if (changeToUnit === "Celsius") {
+    apiUnit = "imperial";
+  }
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capitalizeCity}&appid=${apiKey}&units=${apiUnit}`;
   axios.get(apiUrl).then(changeInfo);
 }
 
 let form = document.querySelector("form");
 form.addEventListener("submit", searchCity);
+
+//temp change
+let currentTempUnit = "Celsius";
+function updateUnit(currTemp, newUnit) {
+  if (newUnit === "Fahrenheit") {
+    return Math.round(currTemp * (9 / 5) + 32);
+  } else {
+    return Math.round((currTemp - 32) * (5 / 9));
+  }
+}
+
+function changeUnit(event) {
+  let changeCurrentTemp = document.querySelector("#current-temp");
+  let changeFeelsLike = document.querySelector("#feels-like-temp");
+  let changeWindSpeed = document.querySelector("#wind-speed");
+  let currentTempNumber = changeCurrentTemp.innerHTML;
+  let feelsLikeTempNumber = changeFeelsLike.innerHTML;
+  let windSpeedNumber = changeWindSpeed.innerHTML;
+  currentTempNumber = currentTempNumber.substring(
+    0,
+    currentTempNumber.length - 2
+  );
+  feelsLikeTempNumber = feelsLikeTempNumber.substring(
+    0,
+    feelsLikeTempNumber.length - 2
+  );
+  if (currentTempUnit === "Celsius") {
+    windSpeedNumber = windSpeedNumber.substring(0, windSpeedNumber.length - 5);
+    currentTempUnit = "Fahrenheit";
+    let newCurrentTemp = updateUnit(currentTempNumber, currentTempUnit);
+    let newFeelsLike = updateUnit(feelsLikeTempNumber, currentTempUnit);
+    let newWindSpeed = Math.round(windSpeedNumber / 1.609);
+    changeCurrentTemp.innerHTML = `${newCurrentTemp}°F`;
+    changeFeelsLike.innerHTML = `${newFeelsLike}°F`;
+    changeWindSpeed.innerHTML = `${newWindSpeed} mph`;
+    tempChange.innerHTML = "Celsius";
+  } else {
+    currentTempUnit = "Celsius";
+    windSpeedNumber = windSpeedNumber.substring(0, windSpeedNumber.length - 4);
+    let newCurrentTemp = updateUnit(currentTempNumber, currentTempUnit);
+    let newFeelsLike = updateUnit(feelsLikeTempNumber, currentTempUnit);
+    let newWindSpeed = Math.round(windSpeedNumber * 1.609);
+    changeCurrentTemp.innerHTML = `${newCurrentTemp}°C`;
+    changeFeelsLike.innerHTML = `${newFeelsLike}°C`;
+    changeWindSpeed.innerHTML = `${newWindSpeed} km/h`;
+    tempChange.innerHTML = "Fahrenheit";
+  }
+}
+let tempChange = document.querySelector("#temp-change");
+tempChange.addEventListener("click", changeUnit);
